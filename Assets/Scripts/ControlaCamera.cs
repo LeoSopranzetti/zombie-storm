ea1433639e43bad14285baebf235a966ebf26402
Leonardo <leo.sopranzetti@hotmail.com>
2023-09-16 11:21:43 -0300

    musica menu

--- Assets/Scripts/ControlaCamera.cs
+++ Assets/Scripts/ControlaCamera.cs
@@ -6,11 +6,13 @@ public class ControlaCamera : MonoBehaviour {
 
     public GameObject Jogador;
     private Vector3 distCompensar;
+    public AudioClip cjHereWeGoAgain;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         distCompensar = transform.position - Jogador.transform.position;
-	}
+        ControlaAudio.instancia.PlayOneShot(cjHereWeGoAgain);
+    }
 	
 	// Update is called once per frame
 	void Update () {
